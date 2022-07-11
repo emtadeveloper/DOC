85df8b232b7f753ddda929ac3deb202d2593efbb
e.taghipour <e.taghipour@rahkar.co>
2022-07-11 13:26:51 +0430

    change 8

--- 10-SLS/Algoritm/BubbleSort.js
+++ 10-SLS/Algoritm/BubbleSort.js
@@ -3,7 +3,7 @@ function sort(arr) {
     for (let outer = 0; outer < resultArray.length; outer++) {
         let outerEl = resultArray[outer]
         for (let inner = outer + 1; inner < resultArray.length; inner++) {
-            let innerEl =  resultArray[inner]
+            let innerEl = resultArray[inner]
             if (outerEl > innerEl) {
                 resultArray[outer] = innerEl;
                 resultArray[inner] = outerEl
